89c9c041209b0aec3f78831d7adc25367fd54ba9
Yehia.Ibrahim <yehia.ibrahim@intercom.com.eg>
2022-08-21 13:20:02 +0200

    fix lang issue

---
+++ src/main/java/com/wellware/osara/config/MvcConfig.java
@@ -0,0 +1,70 @@
+package com.wellware.osara.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Locale;
+
+@Configuration
+@ComponentScan(basePackages = "com.wellware.osara")
+public class MvcConfig implements WebMvcConfigurer {
+
+    private static final String CHARACTER_ENCODING = "UTF-8";
+    private static final String MESSAGE_SOURCE = "classpath:/i18n/message";
+    private static final String VIEWS = "classpath:/templates/";
+    private static final String RESOURCES_LOCATION = "/resources/";
+    private static final String RESOURCES_HANDLER = RESOURCES_LOCATION + "**";
+
+    private WebRequestInterceptor webRequestInterceptor;
+
+
+    public MvcConfig(WebRequestInterceptor webRequestInterceptor) {
+        this.webRequestInterceptor = webRequestInterceptor;
+    }
+
+
+    @Bean(name = "messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(MESSAGE_SOURCE);
+        messageSource.setCacheSeconds(5);
+        messageSource.setDefaultEncoding(CHARACTER_ENCODING);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(
+                new Locale("ar"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+
+
+}
